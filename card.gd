5656e276df38352f71528a48ce5c9a222864a8c1
smacatwood <150390037+smacatwood@users.noreply.github.com>
2024-02-09 19:29:20 -0500

    Add files via upload
    I keep breaking things so I'm keeping it really basic until we get the player hand sorted

---
+++ card.gd
@@ -0,0 +1,50 @@
+extends Sprite2D
+var suit
+var value
+var id 
+
+var textureURLs = [
+"res://assets/cards/English_pattern_9_of_hearts.svg.png",
+"res://assets/cards/English_pattern_10_of_hearts.svg.png",
+"res://assets/cards/English_pattern_jack_of_hearts.svg.png",
+"res://assets/cards/English_pattern_queen_of_hearts.svg.png",
+"res://assets/cards/English_pattern_king_of_hearts.svg.png",
+"res://assets/cards/English_pattern_ace_of_hearts.svg.png",
+"res://assets/cards/English_pattern_9_of_diamonds.svg.png",
+"res://assets/cards/English_pattern_10_of_diamonds.svg.png",
+"res://assets/cards/English_pattern_jack_of_diamonds.svg.png",
+"res://assets/cards/English_pattern_queen_of_diamonds.svg.png",
+"res://assets/cards/English_pattern_king_of_diamonds.svg.png",
+"res://assets/cards/English_pattern_ace_of_diamonds.svg.png",
+"res://assets/cards/English_pattern_9_of_clubs.svg.png",
+"res://assets/cards/English_pattern_10_of_clubs.svg.png",
+"res://assets/cards/English_pattern_jack_of_clubs.svg.png",
+"res://assets/cards/English_pattern_queen_of_clubs.svg.png",
+"res://assets/cards/English_pattern_king_of_clubs.svg.png",
+"res://assets/cards/English_pattern_ace_of_clubs.svg.png",
+"res://assets/cards/English_pattern_9_of_spades.svg.png",
+"res://assets/cards/English_pattern_10_of_spades.svg.png",
+"res://assets/cards/English_pattern_jack_of_spades.svg.png",
+"res://assets/cards/English_pattern_queen_of_spades.svg.png",
+"res://assets/cards/English_pattern_king_of_spades.svg.png",
+"res://assets/cards/English_pattern_ace_of_spades.svg.png"
+]
+
+func setTexture():
+	texture = load(textureURLs[id])
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func selectCard():
+	# card playing logic here
+		emit_signal("cardSelected", self)
+
+func playCard():
+	# card playing logic here
+		emit_signal("cardPlayed", self)
