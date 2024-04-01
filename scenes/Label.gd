13ba91f816e7149a7394c4080644ce385bf51867
River Dyke <noahsemailaccount@gmail.com>
2024-04-01 18:50:30 -0400

    all suits can now be chosen

--- scenes/Label.gd
+++ scenes/Label.gd
@@ -7,24 +7,16 @@ var x
 var id
 var texture
 var texture_rect 
-var textureURLs = [
-	"res://assets/cards/Hearts.png",
-	"res://assets/cards/Clubs.png",
-	"res://assets/cards/Diamonds.png",
-	"res://assets/cards/Spades.png"
-	]
-
+var load_icon = preload("res://scenes/suitIcon.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_text(str("waiting on player 1..."))
 	#set_text(str(meow))
 	global.trumpselected.connect(meowplz)
+	global.suitChosen.connect(woofplz)
 	x=false
 	
-	 # Replace with function body.
-func setTexture():
-	texture = load(textureURLs[id])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -38,35 +30,29 @@ func _on_buttoncard_2_pressed():
 		global.emit_signal("delete")
 		set_text("what would like trump to be?")
 		$Timer.stop()
-		for i in range(textureURLs.size()):
-			var texture_rect = TextureRect.new()
-			texture_rect.texture = load(textureURLs[i])
-			texture_rect.set_size(Vector2(100, 150))
+		for i in range(4):
+			var texture_rect = load_icon.instantiate()
+			texture_rect.woof = i
+			texture_rect.setTexture()
+			#texture_rect.set_size(Vector2(100, 150))
+			texture_rect.scale = Vector2(2,2)
 			texture_rect.position = Vector2(i *170 , 160)
 			add_child(texture_rect)
 		#where all the thingies go
 	pass
 	
-func _unhandled_input(texture):
-	if texture is InputEventMouseButton and texture.pressed and texture.button_index == MOUSE_BUTTON_LEFT:
-		print(texture)
-		for i in range(textureURLs.size()):
-			match textureURLs[i]:
-				"res://assets/cards/Hearts.png":
-					set_text("hearts is trump")
-					print(texture)
-				"res://assets/cards/Clubs.png":
-					set_text("clubs")
-					print(texture)
-				"res://assets/cards/Diamonds.png":
-					set_text("diamonds")
-					print(texture + "diamonds")
-					
-				"res://assets/cards/Spades.png":
-					set_text("spades")
-					print(texture +"spades")
-					
-			break
+func woofplz(suit):
+	match (suit.woof):
+		0: 
+			set_text("hearts is trump")
+		1: 
+			set_text("clubs is trump")
+		2: 
+			set_text("diamonds is trump")
+		3: 
+			set_text("spades is trump")
+	print(suit)
+	pass
 		
 		#the 
 func meowplz(suits):
---
+++ scripts/suitIcon.gd
@@ -0,0 +1,27 @@
+extends Sprite2D
+
+var woof = 0
+var textureURLs = [
+	"res://assets/cards/Hearts.png",
+	"res://assets/cards/Clubs.png",
+	"res://assets/cards/Diamonds.png",
+	"res://assets/cards/Spades.png"
+	]
+
+func _unhandled_input(texture):
+	if texture is InputEventMouseButton and texture.pressed and texture.button_index == MOUSE_BUTTON_LEFT:
+		if get_rect().has_point(to_local(texture.position)):
+			global.emit_signal("suitChosen", self)
+			print("arf arf bark")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func setTexture():
+	texture = load(textureURLs[woof])
